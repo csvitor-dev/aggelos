56c03b8bacfa43fb9bb28470bb26be040bd33319
csvitor-dev <csvitor.dev@gmail.com>
2025-01-17 15:28:34 -0300

    refactor: update the entry point to handle middlwares and routes

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from "hono";
+import { appCors } from "./middlewares/app-cors";
+import messagesRoute from "./routes/messages-route";
 
 const app = new Hono();
 
+app.use("*", appCors());
+
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.json("health check");
 });
+
+app.route("/messages", messagesRoute);
 
 export default app;
