57f921a6b38589d1479325f56b5f399823d74d01
csvitor-dev <csvitor.dev@gmail.com>
2025-01-17 23:53:12 -0300

    refactor: improves home page with apresentation messages list

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div>Hello Next!</div>;
+import MessageList from "@/components/message-list";
+import { RedirectButton } from "@/components/redirect-button";
+
+export default async function HomePage() {
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="text-2xl font-bold mb-4">Messages</h1>
+        <RedirectButton
+          icon="send"
+          href="/new-message"
+          className="bg-emerald-600 hover:bg-emerald-500"
+        >
+          Add New Message
+        </RedirectButton>
+      </div>
+      <MessageList />
+    </div>
+  );
 }
