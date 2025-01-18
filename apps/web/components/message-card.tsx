f6608432df0c9b7ff25efba5ddf5de018c9cbe83
csvitor-dev <csvitor.dev@gmail.com>
2025-01-17 22:31:12 -0300

    refactor(web): segregates `Message` schema

--- apps/web/components/message-card.tsx
+++ apps/web/components/message-card.tsx
@@ -1,10 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-
-interface Message {
-  id: number;
-  author: string;
-  body: string;
-}
+import type { Message } from "@/lib/types/message";
 
 interface MessageCardProps {
   message: Message;
